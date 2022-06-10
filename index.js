0fbf246f3f3e7a2f5e6eb044690aa0e27fb46fdc
S4mm4el <homer1996@gmail.com>
2022-06-10 16:00:34 +0300

    fix: theme changer function

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const body = document.querySelector('body');
 const section = document.querySelectorAll('section');
 const links = document.querySelectorAll('a');
-const light = document.getElementById('light');
-const dark = document.getElementById('dark');
-
 
 console.log('My mark is 120, all done');
 
+// Theme changer
+const light = document.getElementById('light');
+const dark = document.getElementById('dark');
 
 light.addEventListener ('click', () => {
   body.style.color = 'var(--color-black)';
   section.forEach(el => {
     el.style.backgroundColor = 'var(--color-white)'});
+  for (let i = 6; i < links.length; i++){
+    links[i].style.color = 'var(--color-black)';
+  };  
   dark.classList.remove('active');
   light.classList.add('active');
 });
@@ -21,7 +24,12 @@ dark.addEventListener ('click', () => {
     el.style.backgroundColor = 'var(--color-black)'});
   for (let i = 6; i < links.length; i++){
     links[i].style.color = 'var(--color-white)';
-  }
+  };
   light.classList.remove('active');
   dark.classList.add('active');
-})
+});
+
+// Language changer 
+const eng = document.getElementById('eng');
+const rus= document.getElementById('rus');
+const bel = document.getElementById('bel');
