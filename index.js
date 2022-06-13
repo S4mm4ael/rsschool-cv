00b296f239e1764054e47267a1a7e1a6a9762b29
S4mm4el <homer1996@gmail.com>
2022-06-13 16:48:19 +0300

    feat: implement translate function

--- index.js
+++ index.js
@@ -30,6 +30,27 @@ dark.addEventListener ('click', () => {
 });
 
 // Language changer 
+import i18Obj from './assets/js/translate.js';
 const eng = document.getElementById('eng');
 const rus= document.getElementById('rus');
 const bel = document.getElementById('bel');
+
+function translate(lng) {
+  const i18nList = document.querySelectorAll('[data-i18]');
+  i18nList.forEach(el => el.textContent = i18Obj[lng][el.dataset.i18]);
+};
+eng.addEventListener('click', () => translate('eng'));
+rus.addEventListener('click', () => translate('rus'));
+bel.addEventListener('click', () => translate('bel'));
+
+function changeClass (event) {
+  eng.classList.remove('active');
+  rus.classList.remove('active');
+  bel.classList.remove('active');
+  this.classList.add('active');
+}
+
+eng.addEventListener('click', changeClass);
+rus.addEventListener('click', changeClass);
+bel.addEventListener('click', changeClass);
+
